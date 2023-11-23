a578b9ddc804e17c24daf765576fd63716207511
Luminita <124327375+LiXiu37YaHua@users.noreply.github.com>
2023-11-22 22:13:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
-﻿
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lumie Website</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 20px;
+            background-color: lightpink; 
+        }
+
+        header {
+            text-align: center;
+            padding: 10px;
+            background-color: blue; 
+        }
+
+        section {
+            margin-top: 20px;
+        }
+
+        footer {
+            text-align: center;
+            padding: 10px;
+            background-color: blue;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+
+    <header>
+        <h1>Welcome to My Lumi Website</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#links">Links</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home">
+        <h2>Home</h2>
+        <!-- Your home content goes here -->
+    </section>
+
+    <section id="about">
+        <h2>About Me</h2>
+        <p>Lumi website created using HTML and CSS.</p>
+    </section>
+
+    <section id="contact">
+        <h2>Contact</h2>
+        <!-- Your contact content goes here -->
+    </section>
+
+    <section id="links">
+        <h2>Links</h2>
+        <div>
+            <a href="https://www.linkedin.com/in/marissa-lanza-920a6029b/" target="_blank">LinkedIn</a>
+            <a href="https://github.com/LiXiu37YaHua" target="_blank">GitHub</a>
+            <a href="https://snhu.joinhandshake.com/stu/users/35628665" target="_blank">Handshake</a>
+            <a href="https://profile.indeed.com/resume" target="_blank">Indeed</a>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Lumi Website. All rights reserved.</p>
+    </footer>
+
+</body>
+</html>
